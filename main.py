2b520a1de98c129ef1539acf02f9f271c0da89f9
Nevari164 <85701149+Nevari164@users.noreply.github.com>
2024-12-01 06:23:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 # Initialize Scratch cloud connection
 session = scratch.login("Prettingminecraft", "5arkansas6")
 project = session.connect_cloud(1097165205)  # Replace with your project ID
-offset = 49567
+offset = 60000
 # Global control variable
 should_update = True
 start_time = time.time()
